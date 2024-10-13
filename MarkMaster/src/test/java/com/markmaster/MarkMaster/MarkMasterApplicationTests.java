e783c88ebbca64f53ebf2d7512b51741065e5f75
Rohith <rohithk652@gmail.com>
2024-10-13 10:32:37 +0530

    Add initial project files

---
+++ MarkMaster/src/test/java/com/markmaster/MarkMaster/MarkMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.markmaster.MarkMaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
